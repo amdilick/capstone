63abe889825b1443bc38fbc2d198f5c5372e66c3
dilicka <annette.dilick@vumc.org>
2020-08-16 18:30:37 -0500

    removed duplicated code

--- InitDataSubsetting.R
+++ InitDataSubsetting.R
@@ -1,32 +1,15 @@
 
-X2019publicTX <- subset(X2019publiclarcsv, state_code== 'TX')
-write.csv(X2019publicTX, "X2019publicTX.csv", row.names = TRUE)
-remove(X2019publicTX)
-
 X2019publicTX_allColumns <- subset(X2019publiclarcsv, state_code== 'TX')
 write.csv(X2019publicTX_allColumns, "X2019publicTX_allColumns.csv", row.names = TRUE)
 remove(X2019publicTX_allColumns)
-
-
-X2019publicTN <- subset(X2019publiclarcsv, state_code== 'TN')
-write.csv(X2019publicTN, "X2019publicTN.csv", row.names = TRUE)
-remove(X2019publicTN)
 
 X2019publicTN_allColumns <- subset(X2019publiclarcsv, state_code== 'TN')
 write.csv(X2019publicTN_allColumns, "X2019publicTN_allColumns.csv", row.names = TRUE)
 remove(X2019publicTN_allColumns)
 
-X2019publicLA <- subset(X2019publiclarcsv, state_code== 'LA')
-write.csv(X2019publicLA, "X2019publicLA.csv", row.names = TRUE)
-remove(X2019publicLA)
-
 X2019publicLA_allColumns <- subset(X2019publiclarcsv, state_code== 'LA')
 write.csv(X2019publicLA_allColumns, "X2019publicLA_allColumns.csv", row.names = TRUE)
 remove(X2019publicLA_allColumns)
-
-X2019publicNY <- subset(X2019publiclarcsv, state_code== 'NY')
-write.csv(X2019publicNY, "X2019publicNY.csv", row.names = TRUE)
-remove(X2019publicNY)
 
 X2019publicNY_allColumns <- subset(X2019publiclarcsv, state_code== 'NY')
 write.csv(X2019publicNY_allColumns, "X2019publicNY_allColumns.csv", row.names = TRUE)
