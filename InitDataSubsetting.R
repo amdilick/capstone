f7caa5803980883ac0f0e676c9a69cf1d1599709
dilicka <annette.dilick@vumc.org>
2020-08-16 21:03:23 -0500

    set row.names=FALSE

--- InitDataSubsetting.R
+++ InitDataSubsetting.R
@@ -1,16 +1,16 @@
 
 X2019publicTX_allColumns <- subset(X2019publiclarcsv, state_code== 'TX')
-write.csv(X2019publicTX_allColumns, "X2019publicTX_allColumns.csv", row.names = TRUE)
+write.csv(X2019publicTX_allColumns, "X2019publicTX_allColumns.csv", row.names = FALSE)
 remove(X2019publicTX_allColumns)
 
 X2019publicTN_allColumns <- subset(X2019publiclarcsv, state_code== 'TN')
-write.csv(X2019publicTN_allColumns, "X2019publicTN_allColumns.csv", row.names = TRUE)
+write.csv(X2019publicTN_allColumns, "X2019publicTN_allColumns.csv", row.names = FALSE)
 remove(X2019publicTN_allColumns)
 
 X2019publicLA_allColumns <- subset(X2019publiclarcsv, state_code== 'LA')
-write.csv(X2019publicLA_allColumns, "X2019publicLA_allColumns.csv", row.names = TRUE)
+write.csv(X2019publicLA_allColumns, "X2019publicLA_allColumns.csv", row.names = FALSE)
 remove(X2019publicLA_allColumns)
 
 X2019publicNY_allColumns <- subset(X2019publiclarcsv, state_code== 'NY')
-write.csv(X2019publicNY_allColumns, "X2019publicNY_allColumns.csv", row.names = TRUE)
+write.csv(X2019publicNY_allColumns, "X2019publicNY_allColumns.csv", row.names = FALSE)
 remove(X2019publicNY_allColumns)
