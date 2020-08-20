f1c85452252400a258e98e72d426105347e680bb
LAPTOP-ANNETTE\annet <annette.dilick@gmail.com>
2020-08-19 22:02:58 -0500

    more cleaning

--- TNDataCleaning.R
+++ TNDataCleaning.R
@@ -78,3 +78,64 @@ TN2019 <- subset(TN2019, select=-aus_4)
 TN2019 <- subset(TN2019, select=-aus_5)
 # col count = 59
 
+# remove columns with duplicated loan and lien information 
+TN2019 <- subset(TN2019, select=-loan_type)
+TN2019 <- subset(TN2019, select=-lien_status)
+# col count = 57
+
+# remove columns with duplicated construction and units information
+TN2019 <- subset(TN2019, select=-construction_method)
+TN2019 <- subset(TN2019, select=-total_units)
+# col count = 55
+
+##########################################################################
+
+# convert action_taken levels into 'approved' or 'denied' based on code
+convert_action <- function(action_taken){
+  if (action_taken == 1){  return('approved')  }
+  else if(action_taken == 2){  return('approved')  }
+  else if (action_taken == 8){  return('approved')  }
+  else if (action_taken == 3){  return('denied')  }
+  else if (action_taken == 7){ return('denied') }
+}
+
+TN2019$Action_Taken <- sapply(TN2019$action_taken,convert_action)
+TN2019$Action_Taken <- as.factor(TN2019$Action_Taken)
+# drop action_taken (lower case) column 
+TN2019 <- subset(TN2019, select= -action_taken)
+
+# convert purchaser_Type levels from numbers to descriptives
+convert_purchaser <- function(purchaser_type){
+  if (purchaser_type == 0){  return('Not applicable')  }
+  else if(purchaser_type == 1){  return('Fannie Mae')  }
+  else if (purchaser_type == 2){  return('Ginnie Mae')  }
+  else if (purchaser_type == 3){  return('Freddie Mac')  }
+  else if (purchaser_type == 4){  return('Farmer Mac')  }
+  else if (purchaser_type == 5){  return('Private securitizer')  }
+  else if (purchaser_type == 6){  return('Commercial or savings bank or savings association')  }
+  else if (purchaser_type == 71){  return('Credit union mortgage company or finance company')  }
+  else if (purchaser_type == 72){  return('Life insurance company')  }
+  else if (purchaser_type == 8){  return('Affiliate institution')  }
+  else if (purchaser_type == 9){  return('Other type of purchaser')  }
+}
+
+TN2019$Purchaser_Type <- sapply(TN2019$purchaser_type,convert_purchaser)
+TN2019$Purchaser_Type<- as.factor(TN2019$Purchaser_Type)
+# drop action_taken (lower case) column 
+TN2019 <- subset(TN2019, select= -purchaser_type)
+
+# convert action_taken levels into 'approved' or 'denied' based on code
+convert_action <- function(action_taken){
+  if (action_taken == 1){  return('approved')  }
+  else if(action_taken == 2){  return('approved')  }
+  else if (action_taken == 8){  return('approved')  }
+  else if (action_taken == 3){  return('denied')  }
+  else if (action_taken == 7){ return('denied') }
+}
+
+TN2019$Action_Taken <- sapply(TN2019$action_taken,convert_action)
+TN2019$Action_Taken <- as.factor(TN2019$Action_Taken)
+# drop action_taken (lower case) column 
+TN2019 <- subset(TN2019, select= -action_taken)
+
+
---
+++ TNExport.R
@@ -0,0 +1,4 @@
+write.csv(TN2019, "TN2019.csv", row.names = FALSE)
+
+contents(TN2019)
+
