de4ed3bb7db0eb0b30151e17a9198046f13e056f
amdilick <annette.dilick@gmail.com>
2020-09-14 22:36:44 -0500

    begin the analysis

--- TNDataAnalysis_pt1.R
+++ TNDataAnalysis_pt1.R
@@ -1,22 +1,31 @@
 # view proportions of each level in factor columns
+TN2019_backup <- TN2019_reg
+
 
 tbl_debt <- table(TN2019_reg$debt_to_income_ratio)
 prop.table(tbl_debt)
+TN2019_reg$Applicant_Ethnicity <- TN2019_reg$Applicant_Ethnicity_1
+TN2019_reg <- subset(TN2019_reg, select=-Applicant_Ethnicity_1)
+TN2019_reg$Co_Applicant_Ethnicity <- TN2019_reg$Co_Applicant_Ethnicity_1
+TN2019_reg <- subset(TN2019_reg, select=-Co_Applicant_Ethnicity_1)
+TN2019_reg$Applicant_Race <- TN2019_reg$Applicant_Race_1
+TN2019_reg <- subset(TN2019_reg, select=-Applicant_Race_1)
+TN2019_reg$Co_Applicant_Race<- TN2019_reg$Co_Applicant_Race_1
+TN2019_reg <- subset(TN2019_reg, select=-Co_Applicant_Race_1)
 
-
-TN2019_X2 <- chiSquare(TN2019$Application_Status ~ TN2019$derived_msa_md +
-      TN2019$derived_loan_product_type + TN2019$derived_dwelling_category +
-      TN2019$derived_ethnicity + TN2019$derived_race + TN2019$derived_sex +
-      TN2019$Occupancy_Type + TN2019$applicant_age + TN2019$Applicant_Sex +
-      TN2019$Loan_Purpose + TN2019$Preapproval + TN2019$AUS_1 + 
-      TN2019$Applicant_Credit_Score_Type)
+TN2019_X2 <- chiSquare(TN2019_reg$Application_Status ~ TN2019_reg$derived_msa_md +
+      TN2019_reg$derived_loan_product_type + TN2019_reg$derived_dwelling_category +
+      TN2019_reg$derived_ethnicity + TN2019_reg$Applicant_Ethnicity +
+      TN2019_reg$Co_Applicant_Ethnicity + TN2019_reg$derived_race + TN2019_reg$Applicant_Race + 
+      TN2019_reg$Co_Applicant_Race + TN2019_reg$derived_sex + 
+      TN2019_reg$applicant_age + TN2019_reg$Applicant_Sex)  
 print(TN2019_X2)
-plot(chiSquare(TN2019$Application_Status ~ TN2019$derived_msa_md +
-                  TN2019$derived_loan_product_type + TN2019$derived_dwelling_category +
-                  TN2019$derived_ethnicity + TN2019$derived_race + TN2019$derived_sex +
-                  TN2019$Occupancy_Type + TN2019$applicant_age + TN2019$Applicant_Sex +
-                  TN2019$Loan_Purpose + TN2019$Preapproval +  
-                  TN2019$Applicant_Credit_Score_Type))
+plot(chiSquare(TN2019_reg$Application_Status ~ TN2019_reg$derived_msa_md +
+      TN2019_reg$derived_loan_product_type + TN2019_reg$derived_dwelling_category +
+      TN2019_reg$derived_ethnicity + TN2019_reg$Applicant_Ethnicity +
+      TN2019_reg$Co_Applicant_Ethnicity + TN2019_reg$derived_race + TN2019_reg$Applicant_Race + 
+      TN2019_reg$Co_Applicant_Race + TN2019_reg$derived_sex + 
+      TN2019_reg$applicant_age + TN2019_reg$Applicant_Sex))
 
 #TN2019_Xval <- subset(TN2019_main, select=-property_value)
 TN2019_Xval <- subset(TN2019_reg, select=-Application_Status)
@@ -37,6 +46,6 @@ fviz_famd_var(TN.famd, choice='quanti.var', col.var='cos2', gradient.cols = c("#
 TN2019_final <- subset(TN2019_main, select =-income)
 TN2019_final <- subset(TN2019_final, select =-Applicant_Ethnicity_1)
 TN2019_final <- subset(TN2019_final, select =-Applicant_Race_1)
-TN2019_final <- subset(TN2019_final, select = -county_code)
+
 
 TN2019_maybe <- subset(TN2019_final, select=c('Application_Status', 'derived_race', 'derived_ethnicity', 'applicant_age','Applicant_Sex' ))
